2fb9a6746e4a580431200a16ca4b09fd864451ae
Mohammad Firmansyah <76118762+dimasdh842@users.noreply.github.com>
2022-06-25 17:34:17 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,5 @@ const webpack = require('webpack');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new webpack.ProvidePlugin({
-        process: "process/browser",
-        Buffer: ["buffer", "Buffer"],
-      }),
-      new webpack.ProvidePlugin({
-        process: "process/browser"
-      }),
-    ],
-  },
 })
 
